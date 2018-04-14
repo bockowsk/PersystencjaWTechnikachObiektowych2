103d1325e36b315f07711c24012ce4b3669416b0
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-04-14 14:41:18 +0200

    updating SchoolClasses working properly

--- src/main/java/pl/edu/agh/ki/mwo/SchoolWebApp/controller/SchoolClassesController.java
+++ src/main/java/pl/edu/agh/ki/mwo/SchoolWebApp/controller/SchoolClassesController.java
@@ -23,77 +23,104 @@ public class SchoolClassesController {
 	@Autowired
 	private SchoolClassRepository schoolClassRepository;
 
-	
-    @RequestMapping(value="/SchoolClasses")
-    public String listSchoolClass(Model model, HttpSession session) {    	
-    	if (session.getAttribute("userLogin") == null)
-    		return "redirect:/Login";
+	@RequestMapping(value = "/SchoolClasses")
+	public String listSchoolClass(Model model, HttpSession session) {
+		if (session.getAttribute("userLogin") == null)
+			return "redirect:/Login";
 
-    	//model.addAttribute("schoolClasses", DatabaseConnector.getInstance().getSchoolClasses());
-    	model.addAttribute("schoolClasses", schoolClassRepository.findAll());
-    	
-        return "schoolClassesList";    
-    }
-    
-    @RequestMapping(value="/AddSchoolClass")
-    public String displayAddSchoolClassForm(Model model, HttpSession session) {    	
-    	if (session.getAttribute("userLogin") == null)
-    		return "redirect:/Login";
+		// model.addAttribute("schoolClasses",
+		// DatabaseConnector.getInstance().getSchoolClasses());
+		model.addAttribute("schoolClasses", schoolClassRepository.findAll());
 
-       	model.addAttribute("schools", schoolRepository.findAll());
-       	
-        return "schoolClassForm";    
-    }
+		return "schoolClassesList";
+	}
 
-    @RequestMapping(value="/CreateSchoolClass", method=RequestMethod.POST)
-    public String createSchoolClass(@RequestParam(value="schoolClassStartYear", required=false) String startYear,
-    		@RequestParam(value="schoolClassCurrentYear", required=false) String currentYear,
-    		@RequestParam(value="schoolClassProfile", required=false) String profile,
-    		@RequestParam(value="schoolClassSchool", required=false) String schoolId,
-    		Model model, HttpSession session) {    	
-    	if (session.getAttribute("userLogin") == null)
-    		return "redirect:/Login";
-    	
-    	SchoolClass schoolClass = new SchoolClass();
-    	schoolClass.setStartYear(Integer.valueOf(startYear));
-    	schoolClass.setCurrentYear(Integer.valueOf(currentYear));
-    	schoolClass.setProfile(profile);
-    	School school=schoolRepository.findById(Long.valueOf(schoolId)).get();
-    	schoolClass.setSchool(school);
-    	
-    	//DatabaseConnector.getInstance().addSchoolClass(schoolClass, schoolId);
-    	schoolClassRepository.save(schoolClass);
-       	model.addAttribute("schoolClasses", schoolClassRepository.findAll());
-    	model.addAttribute("message", "Nowa klasa została dodana");
-         	
-    	return "schoolClassesList";
-    }
-    
-    @RequestMapping(value="/DeleteSchoolClass", method=RequestMethod.POST)
-    public String deleteSchoolClass(@RequestParam(value="schoolClassId", required=false) String schoolClassId,
-    		Model model, HttpSession session) {    	
-    	if (session.getAttribute("userLogin") == null)
-    		return "redirect:/Login";
-    	
-    	//DatabaseConnector.getInstance().deleteSchoolClass(schoolClassId);
-    	schoolClassRepository.deleteById(Long.valueOf(schoolClassId));
-       	model.addAttribute("schoolClasses", schoolClassRepository.findAll());
-    	model.addAttribute("message", "Klasa została usunięta");
-         	
-    	return "schoolClassesList";
-    }
-    
-    @RequestMapping(value="/ShowUpdateSchoolClassForm")
-    public String showUpdateSchoolClassForm(@RequestParam(value = "schoolClassId") String schoolClassId, Model model, HttpSession session) {    	
-    	if (session.getAttribute("userLogin") == null)
-    		return "redirect:/Login";
+	@RequestMapping(value = "/AddSchoolClass")
+	public String displayAddSchoolClassForm(Model model, HttpSession session) {
+		if (session.getAttribute("userLogin") == null)
+			return "redirect:/Login";
 
-    	SchoolClass schoolClass=schoolClassRepository.findById(Long.valueOf(schoolClassId)).get();
-       	model.addAttribute("schoolClass", schoolClass);
-       	model.addAttribute("schools", schoolRepository.findAll());
-       	
-        return "schoolClassUpdateForm";    
-    }
+		model.addAttribute("schools", schoolRepository.findAll());
 
+		return "schoolClassForm";
+	}
+
+	@RequestMapping(value = "/CreateSchoolClass", method = RequestMethod.POST)
+	public String createSchoolClass(@RequestParam(value = "schoolClassStartYear", required = false) String startYear,
+			@RequestParam(value = "schoolClassCurrentYear", required = false) String currentYear,
+			@RequestParam(value = "schoolClassProfile", required = false) String profile,
+			@RequestParam(value = "schoolClassSchool", required = false) String schoolId, Model model,
+			HttpSession session) {
+		if (session.getAttribute("userLogin") == null)
+			return "redirect:/Login";
+
+		SchoolClass schoolClass = new SchoolClass();
+		schoolClass.setStartYear(Integer.valueOf(startYear));
+		schoolClass.setCurrentYear(Integer.valueOf(currentYear));
+		schoolClass.setProfile(profile);
+		School school = schoolRepository.findById(Long.valueOf(schoolId)).get();
+		schoolClass.setSchool(school);
+
+		// DatabaseConnector.getInstance().addSchoolClass(schoolClass, schoolId);
+		schoolClassRepository.save(schoolClass);
+		model.addAttribute("schoolClasses", schoolClassRepository.findAll());
+		model.addAttribute("message", "Nowa klasa została dodana");
+
+		return "schoolClassesList";
+	}
+
+	@RequestMapping(value = "/DeleteSchoolClass", method = RequestMethod.POST)
+	public String deleteSchoolClass(@RequestParam(value = "schoolClassId", required = false) String schoolClassId,
+			Model model, HttpSession session) {
+		if (session.getAttribute("userLogin") == null)
+			return "redirect:/Login";
+
+		// DatabaseConnector.getInstance().deleteSchoolClass(schoolClassId);
+		schoolClassRepository.deleteById(Long.valueOf(schoolClassId));
+		model.addAttribute("schoolClasses", schoolClassRepository.findAll());
+		model.addAttribute("message", "Klasa została usunięta");
+
+		return "schoolClassesList";
+	}
+
+	@RequestMapping(value = "/ShowUpdateSchoolClassForm")
+	public String showUpdateSchoolClassForm(@RequestParam(value = "schoolClassId") String schoolClassId, Model model,
+			HttpSession session) {
+		if (session.getAttribute("userLogin") == null)
+			return "redirect:/Login";
+
+		SchoolClass schoolClass = schoolClassRepository.findById(Long.valueOf(schoolClassId)).get();
+		model.addAttribute("schoolClass", schoolClass);
+		model.addAttribute("schools", schoolRepository.findAll());
+
+		return "schoolClassUpdateForm";
+	}
+
+	@RequestMapping(value = "/UpdateSchoolClass", method = RequestMethod.POST)
+	public String updateSchoolClass(
+			@RequestParam(value = "schoolClassStartYear", required = false) String schoolClassStartYear,
+			@RequestParam(value = "schoolClassCurrentYear", required = false) String schoolClassCurrentYear,
+			@RequestParam(value = "schoolClassProfile") String schoolClassProfile,
+			@RequestParam(value = "schoolClassId") String schoolClassId,
+			@RequestParam(value = "schoolClassSchool") String schoolClassSchool, Model model, HttpSession session) {
+
+		if (session.getAttribute("userLogin") == null)
+			return "redirect:/Login";
+
+		SchoolClass schoolClass = schoolClassRepository.findById(Long.valueOf(schoolClassId)).get();
+		schoolClass.setStartYear(Integer.valueOf(schoolClassStartYear));
+		schoolClass.setCurrentYear(Integer.valueOf(schoolClassCurrentYear));
+		schoolClass.setProfile(schoolClassProfile);
+		
+		School school=schoolRepository.findById(Long.valueOf(schoolClassSchool)).get();
+		
+		schoolClass.setSchool(school);
+		// update
+		schoolClassRepository.save(schoolClass);
+		model.addAttribute("schoolClasses", schoolClassRepository.findAll());
+		model.addAttribute("message", "Klasa zaktualizowana");
+
+		return "schoolClassesList";
+	}
 
 }
